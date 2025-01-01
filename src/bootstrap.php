a90e0175395ce8d70dba352231a8b7d288b926fc
Daniela Kleemann <dk@webcodingcafe.com>
2025-01-01 13:36:06 +0100

    Create bootstrap.php

---
+++ src/bootstrap.php
@@ -0,0 +1,10 @@
+<?php
+
+/*
+ * This file is part of Sweet Blog.
+ *
+ * Copyright (c) 2025 Daniela Kleemann <dk@webcodingcafe.com>
+ * SPDX-License-Identifier: MIT
+ */
+
+declare(strict_types=1);
